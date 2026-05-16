dbe1289a87013a5189a3dcbedf574cae001954d4
Bach Davis <bachaarondavis@gmail.com>
2026-05-15 20:09:02 -0400

    Update main.py

--- lib/main.py
+++ lib/main.py
@@ -42,7 +42,7 @@ while True:
         if step == 1:
             mode = 1
             first = True
-            lvlsel.save(lvlsel.done + 1)
+            lvlsel.save(ran + 1)
         elif step == 2:
             if sound: pygame.mixer.Sound("./audio/thump.mp3").play()
             first = True
